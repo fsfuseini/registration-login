fc028be862ad6c616ada58bd5e3bc3032e09f4ca
fsfuseini <suhuyini86@gmail.com>
2024-09-16 15:40:58 +0000

    first commit

---
+++ script/register.js
@@ -0,0 +1,10 @@
+//Get form elements by id
+
+const registerForm = document.getElementById("register");
+
+// Handle form submit event
+
+registerForm.onsubmit = function (event) {
+    event.preventDefault();
+    console.log(event);
+}
