c90bcd3e178bbb234c6c77501d0e505d7f5c3ada
fsfuseini <suhuyini86@gmail.com>
2024-09-17 15:51:42 +0000

    More javascript

--- script/register.js
+++ script/register.js
@@ -1,10 +1,35 @@
 //Get form elements by id
-
-const registerForm = document.getElementById("register");
-
+const registerForm = document.querySelector("#register");
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //Collect form data
+    const formData = new FormData(registerForm);
+    
+    // Send data to Backend
+    // Display success message to user
+    const messageH1 = document.querySelector("#message");
+    messageH1.textContent = "Account reistered successfully!";
+//   console.log(
+//     formData.get("username"),
+//     formData.get("email"),
+//     formData.get("password")
+//   );
+});
 // Handle form submit event
 
-registerForm.onsubmit = function (event) {
-    event.preventDefault();
-    console.log(event);
+// Declare variable age
+const age = 71;
+// Define square age function
+function squareAge(age) {
+  return age ** 2;
 }
+
+const squaredAge = squareAge(34);
+console.log(squaredAge);
+
+function fname(firstName, middleName, lastName) {
+  return firstName + " " + middleName + " " + lastName;
+}
+
+const fullName = fname("Firdaus", "Suhuyini", "Fuseini");
+console.log(fullName);
